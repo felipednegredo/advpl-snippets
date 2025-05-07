e43b0f547269f26bb37aba35a5da3cd7cf50bca2
Felipe de Negredo <50848514+felipednegredo@users.noreply.github.com>
2025-05-07 12:42:55 +0000

    Atualiza a versão do pacote para 1.7.3 em package.json, package-lock.json e version.txt; modifica a função showServersWebView em extension.js para usar o caminho correto do arquivo servers.html.

--- src/extension.js
+++ src/extension.js
@@ -56,7 +56,7 @@ function showServersWebView(context) {
         jsonData = { configurations: [] };
     }
 
-    const htmlPath = path.join(context.extensionPath, 'webviews', 'servers.html');
+    const htmlPath = path.join(context.extensionPath,'src', 'webviews', 'servers.html');
     const htmlTemplate = fs.readFileSync(htmlPath, 'utf8');
 
     panel.webview.html = htmlTemplate.replace('{{data}}', JSON.stringify(jsonData).replace(/</g, '\\u003c'));
